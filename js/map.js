ebe3b003be77a817eefbfaa05d21156c4f790806
lauragyr <laura.gyr@hotmail.com>
2018-07-04 10:47:45 +0200

    Add files via upload

---
+++ js/map.js
@@ -0,0 +1,6 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibG9lcmloIiwiYSI6ImNqajY4bWx0ZTFybnAzcW0xd2oydnYwemUifQ.GwcVIqZ6ZId6pJF8l13dUg';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v10'
+center: [47.05, 8.79], // starting position as [lng, lat]
+zoom: 13
