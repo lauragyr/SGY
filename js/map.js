ed2556637951365ae1868364329e593f06cb6ac2
lauragyr <laura.gyr@hotmail.com>
2018-07-05 22:22:51 +0200

    test with map

--- js/map.js
+++ js/map.js
@@ -1,6 +1,8 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoibG9lcmloIiwiYSI6ImNqajY4bWx0ZTFybnAzcW0xd2oydnYwemUifQ.GwcVIqZ6ZId6pJF8l13dUg';
 var map = new mapboxgl.Map({
 container: 'map',
-style: 'mapbox://styles/mapbox/streets-v10'
+style: 'mapbox:/styles/mapbox/basic-v9'
+zoom: 50
 center: [47.05, 8.79], // starting position as [lng, lat]
-zoom: 13
+
+});
